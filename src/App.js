0c8751b8bb96e72673bc59fd5592fafbaa64ac3c
Pedro Campelo <pedrohcmatheus@gmail.com>
2020-10-01 16:08:24 -0300

    first build of Turu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect} from 'react';
 
-function App() {
+// class component
+//
+// class App extends React.Component {
+//   render() {
+//
+//   }
+// }
+
+// a different
+// function App() { // functional component
+//   return (
+//     <div className="App">
+//       <h1> Hello World! </h1>
+//     </div>
+//   );
+// }
+
+// const App = () => (
+//     <div className="App">
+//       <h1> Hello World! </h1>
+//     </div>
+// )
+
+const App = () => {
+  const [newTodo, setNewTodo] = useState(''); // returns an array with two values;
+  const [todos, setTodos] = useState([])
+
+  const onNewTodoChange = useCallback((event) => {
+    setNewTodo(event.target.value)
+  }, []);
+
+  const formSubmitted = useCallback((event) => {
+    event.preventDefault();
+    // todos.push({}) React wouldn't know the array has changed.
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        content: newTodo,
+        done: false
+      }
+    ])
+  }, [newTodo, todos]);
+
+  useEffect(() => {
+
+    console.log('todos', todos)
+    
+  }, [todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <form onSubmit={formSubmitted}>
+          <label htmlFor='newTodo'>Enter a todo</label>
+          <input
+            id='newTodo'
+            name='newTodo'
+            value={newTodo}
+            onChange={onNewTodoChange}
+          />
+          <button>Add Todo</button>
+        </form>
+      </div>
+  )
 }
 
 export default App;
